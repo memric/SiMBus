dafd609c4be4df1e8f5307666ad197cbff9f7d7a
Valeriy Chudnikov <memricpf@gmail.com>
2023-02-20 18:41:47 +0300

    Regs generator fixes

--- RegGen/RegGen.py
+++ RegGen/RegGen.py
@@ -11,17 +11,17 @@ from rich.console import Console
 from rich.table import Table
 
 REG_MIN_VALUE = 0
-REG_MAX_VALUE = 65535
+REG_MAX_VALUE = 0xFFFF
 
 #Converts string to int value
-def str_tield2int(field):
+def str_field2int(field):
     try:
         if field.startswith('0x'):
             val = int(field.replace("0x",""), 16)
         else:
             val = int(field)
     except ValueError:
-        val = 0
+        val = -1
     return val
 
 #Adds separation between str1 and str2 to aling str2 to pos
@@ -69,39 +69,39 @@ def main(argv=None): # IGNORE:C0111
         '''Check CSV table content'''
         try:
             for row in reader:
-                # if row['Name'].upper() == 'RESERVED':
-                #     continue
+                if row['Name'].upper() == 'RESERVED':
+                    console.print("[yellow]Warning: Skip RESERVED register")
+                    continue
                 
-                addr = int(row['Address'])
+                addr = str_field2int(row['Address'])
                 if addr < REG_MIN_VALUE or addr > REG_MAX_VALUE:
-                    console.print("[red]Error: Address of register \"%s\" is not correct"%(row['Name']))
-                    sys.exit(-1)
+                    addr = 0
+                    console.print("[yellow]Warning: Address of register \"%s\" is not correct. Set to 0"%(row['Name']))
                     
-                min = str_tield2int(row['Min'])
+                min = str_field2int(row['Min'])
                 if min < REG_MIN_VALUE or min > REG_MAX_VALUE:
-                    console.print("[red]Error: Minimum value of register \"%s\" is not correct"%(row['Name']))
-                    sys.exit(-1)
+                    min = 0
+                    console.print("[yellow]Warning: Minimum value of register \"%s\" is not correct. Set to 0"%(row['Name']))
                     
-                max = str_tield2int(row['Max'])
+                max = str_field2int(row['Max'])
                 if max < REG_MIN_VALUE or max > REG_MAX_VALUE:
-                    console.print("[red]Error: Maximum value of register \"%s\" is not correct"%(row['Name']))
-                    sys.exit(-1)
+                    max = 0xFFFF
+                    console.print("[yellow]Warning: Maximum value of register \"%s\" is not correct. Set to 0xFFFF"%(row['Name']))
                     
                 if min >= max:
                     console.print("[yellow]Warning: Minimum value of register \"%s\" is equal or greiter than maximum value"%(row['Name']))
                     
-                default = str_tield2int(row['Default'])
+                default = str_field2int(row['Default'])
                 if default < REG_MIN_VALUE or default > REG_MAX_VALUE:
-                    console.print("[red]Error: Default value of register \"%s\" is not correct"%(row['Name']))
-                    sys.exit(-1)
+                    default = 0
+                    console.print("[yellow]Warning: Default value of register \"%s\" is not correct. Set to 0"%(row['Name']))
                     
                 if row['Mode'].upper() == 'R' or row['Mode'].upper() == 'W' or row['Mode'].upper() == 'RW' or row['Mode'].upper() == 'R/W':
                     oper = row['Mode'].upper()
                 else:
-                    print("Error: Mode of register \"%s\" is not correct"%(row['Name']))
+                    oper = 'R'
+                    console.print("[yellow]Warning: Mode of register \"%s\" is not correct. Set to R"%(row['Name']))
          
-                # print("Register: \"%s\"; Addr: %s; Min: %d; Max: %d; Default: %d; Oper: %s"%(row['Name'], hex(addr), min, max, default, oper))
-                
                 reg_map.append({'Address':addr, 'Min':min, 'Max':max, 'Default':default, 'Mode':oper, 'Name':row['Name'], 'Comment':row['Comment']})
                 
                 if addr > last_reg_addr:
@@ -119,122 +119,122 @@ def main(argv=None): # IGNORE:C0111
         
         reg_map = sorted(reg_map, key=lambda k: k['Address'])
 
-    '''Create Table'''
-    table = Table(title="[bold]Registers map")
+        '''Create Table'''
+        table = Table(title="[bold]Registers map")
 
-    table.add_column("Address", style="cyan", no_wrap=True)
-    table.add_column("Caption", style="green")
-    table.add_column("Min")
-    table.add_column("Max")
-    table.add_column("Default")
-    table.add_column("Mode")
+        table.add_column("Address", style="cyan", no_wrap=True)
+        table.add_column("Caption", style="green")
+        table.add_column("Min")
+        table.add_column("Max")
+        table.add_column("Default")
+        table.add_column("Mode")
 
-    for row in reg_map:
-        #Fill Console table
-        table.add_row(hex(row['Address']), row['Name'], str(min), str(max), str(default), oper)
+        for row in reg_map:
+            #Fill Console table
+            table.add_row(hex(row['Address']), row['Name'], str(row['Min']), str(row['Max']), str(row['Default']), row['Mode'])
 
-    console.print(table)
+        console.print(table)
         
-    '''Create C files'''
-        
-    '''Register map'''
-    #open template file
-    with open("mb_regs_h.template") as ht:
-        rmh_template = string.Template(ht.read())
-        
-    rmh_f = open('mb_regs.h', 'w', newline='')
-    
-    reg_map_defs = ""
-    #get closest tab position index
-    tab_pos_ind = 4*((max_name_len+ 17 + 3)//4) + 4;
-    
-    for row in reg_map:
-        reg_name = row['Name'].upper()
-        reg_name = reg_name.replace(' ','_')
-        
-        if reg_name == 'RESERVED':
-            continue
-        
-        addr = row['Address']
-        min = row['Min']
-        max = row['Max']
-        default = row['Default']
-        oper = row['Mode']
-        
-        if oper == 'R':
-            oper_mode = 'REG_OPT_R_ONLY'
-        elif oper == 'W':
-            oper_mode = 'REG_OPT_WR_ONLY'
-        elif oper == 'RW' or oper == 'R/W':
-            oper_mode = 'REG_OPT_ALL'
+        '''Create C files'''
             
-        reg_map_defs += "/* Register: %s\r\n* Addr: %s; Min: %d; Max: %d; Default: %d; Oper: %s */\r\n"%(row['Comment'], hex(addr), min, max, default, oper)
-        reg_map_defs += tab2pos("#define REG_%s_ADDR"%(reg_name), "%s\r\n"%(hex(addr)), tab_pos_ind)
-        reg_map_defs += tab2pos("#define REG_%s_MIN"%(reg_name), "%s\r\n"%(min), tab_pos_ind)
-        reg_map_defs += tab2pos("#define REG_%s_MAX"%(reg_name), "%s\r\n"%(max), tab_pos_ind)
-        reg_map_defs += tab2pos("#define REG_%s_DEF"%(reg_name), "%s\r\n"%(default), tab_pos_ind)
-        reg_map_defs += tab2pos("#define REG_%s_OPT"%(reg_name), "%s\r\n"%(oper_mode), tab_pos_ind)
-        reg_map_defs += "\r\n"
-    
-    #fill template and write to file        
-    rmh_content = rmh_template.safe_substitute(date=datetime.date.today(), \
-                                               register_map = reg_map_defs, \
-                                               reg_last_addr = hex(last_reg_addr), \
-                                               reg_num = reg_num)
-    rmh_f.write(rmh_content)
-    
-    console.print("[green]File mb_regs.h is created")
-    
-    '''Registers functions source file'''
-    #open template file
-    with open("mb_regs_c.template") as ht:
-        mbr_template = string.Template(ht.read())
+        '''Register map'''
+        #open template file
+        with open("mb_regs_h.template") as ht:
+            rmh_template = string.Template(ht.read())
+            
+        rmh_f = open('mb_regs.h', 'w', newline='')
         
-    mbr_f = open('mb_regs.c', 'w', newline='')
+        reg_map_defs = ""
+        #get closest tab position index
+        tab_pos_ind = 4*((max_name_len+ 17 + 3)//4) + 4;
+        
+        for row in reg_map:
+            reg_name = row['Name'].upper()
+            reg_name = reg_name.replace(' ','_')
+            
+            if reg_name == 'RESERVED':
+                continue
+            
+            addr = row['Address']
+            min = row['Min']
+            max = row['Max']
+            default = row['Default']
+            oper = row['Mode']
+            
+            if oper == 'R':
+                oper_mode = 'REG_OPT_R_ONLY'
+            elif oper == 'W':
+                oper_mode = 'REG_OPT_WR_ONLY'
+            elif oper == 'RW' or oper == 'R/W':
+                oper_mode = 'REG_OPT_ALL'
+                
+            reg_map_defs += "/* Register: %s\r\n* Addr: %s; Min: %d; Max: %d; Default: %d; Oper: %s */\r\n"%(row['Comment'], hex(addr), min, max, default, oper)
+            reg_map_defs += tab2pos("#define REG_%s_ADDR"%(reg_name), "%s\r\n"%(hex(addr)), tab_pos_ind)
+            reg_map_defs += tab2pos("#define REG_%s_MIN"%(reg_name), "%s\r\n"%(min), tab_pos_ind)
+            reg_map_defs += tab2pos("#define REG_%s_MAX"%(reg_name), "%s\r\n"%(max), tab_pos_ind)
+            reg_map_defs += tab2pos("#define REG_%s_DEF"%(reg_name), "%s\r\n"%(default), tab_pos_ind)
+            reg_map_defs += tab2pos("#define REG_%s_OPT"%(reg_name), "%s\r\n"%(oper_mode), tab_pos_ind)
+            reg_map_defs += "\r\n"
+        
+        #fill template and write to file
+        rmh_content = rmh_template.safe_substitute(date=datetime.date.today(), \
+                                                   register_map = reg_map_defs, \
+                                                   reg_last_addr = hex(last_reg_addr), \
+                                                   reg_num = reg_num)
+        rmh_f.write(rmh_content)
+        
+        console.print("[green]File mb_regs.h is created")
+        
+        '''Registers functions source file'''
+        #open template file
+        with open("mb_regs_c.template") as ht:
+            mbr_template = string.Template(ht.read())
+            
+        mbr_f = open('mb_regs.c', 'w', newline='')
 
-    #static variables
-    
-    #fill geristers options array
-    reg_opts_vals = ""
-    for row in reg_map:
-        reg_name = row['Name'].upper()
-        reg_name = reg_name.replace(' ','_')
+        #static variables
         
-        reg_opt_str = ''
+        #fill geristers options array
+        reg_opts_vals = ""
+        for row in reg_map:
+            reg_name = row['Name'].upper()
+            reg_name = reg_name.replace(' ','_')
+            
+            reg_opt_str = ''
+            
+            if reg_name == 'RESERVED':
+                reg_opt_str += "\t{REG_OPT_R_ONLY, 0, 0, 0}"
+            else:
+                reg_opt_str += \
+                "\t{REG_%s_OPT, REG_%s_MIN, REG_%s_MAX, REG_%s_DEF}"%(reg_name, \
+                                                                      reg_name, \
+                                                                      reg_name, \
+                                                                      reg_name)
+            if row != reg_map[-1]:
+                reg_opt_str += ",\r\n"
+            
+            reg_opts_vals += reg_opt_str
         
-        if reg_name == 'RESERVED':
-            reg_opt_str += "\t{REG_OPT_R_ONLY, 0, 0, 0}"
-        else:
-            reg_opt_str += \
-            "\t{REG_%s_OPT, REG_%s_MIN, REG_%s_MAX, REG_%s_DEF}"%(reg_name, \
-                                                                  reg_name, \
-                                                                  reg_name, \
-                                                                  reg_name)
-        if row != reg_map[-1]:
-            reg_opt_str += ",\r\n"
+        #fill geristers values array
+        reg_def_vals = ""
+        for row in reg_map:
+            reg_name = row['Name'].upper()
+            reg_name = reg_name.replace(' ','_')
+            
+            if reg_name == 'RESERVED':
+                reg_def_vals += "\t0"
+            else:
+                reg_def_vals += "\tREG_%s_DEF"%(reg_name)
+            if row != reg_map[-1]:
+                reg_def_vals += ",\r\n"
+         
+        #fill template and write to file
+        mbr_content = mbr_template.safe_substitute(date=datetime.date.today(), \
+                                                   opt_vals = reg_opts_vals, \
+                                                   def_vals = reg_def_vals)
+        mbr_f.write(mbr_content)
         
-        reg_opts_vals += reg_opt_str
-    
-    #fill geristers values array
-    reg_def_vals = ""
-    for row in reg_map:
-        reg_name = row['Name'].upper()
-        reg_name = reg_name.replace(' ','_')
-        
-        if reg_name == 'RESERVED':
-            reg_def_vals += "\t0"
-        else:
-            reg_def_vals += "\tREG_%s_DEF"%(reg_name)
-        if row != reg_map[-1]:
-            reg_def_vals += ",\r\n"
-     
-    #fill template and write to file        
-    mbr_content = mbr_template.safe_substitute(date=datetime.date.today(), \
-                                               opt_vals = reg_opts_vals, \
-                                               def_vals = reg_def_vals)
-    mbr_f.write(mbr_content)
-    
-    console.print("[green]File mb_regs.c is created")
+        console.print("[green]File mb_regs.c is created")
             
 if __name__ == "__main__":
     sys.exit(main())
